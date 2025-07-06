84ac6b2241922d40d9d4e934013fb1d468ffff17
leonardo ferreira <leodsmf@gmail.com>
2025-07-05 21:17:28 -0300

    configuring MSW

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom/vitest'
+import { server } from './mocks/node'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
